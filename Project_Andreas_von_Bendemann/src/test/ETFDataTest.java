99da3dd1effcbcdb1653106ae6c003a50b875f48
andreasvb20 <128102407+andreasvb20@users.noreply.github.com>
2023-04-30 18:17:08 +0200

    Update ETFDataTest.java

--- Project_Andreas_von_Bendemann/src/test/ETFDataTest.java
+++ Project_Andreas_von_Bendemann/src/test/ETFDataTest.java
@@ -1,12 +1,40 @@
 package test;
 
+import types.ETFData;
 import types.ETFDataFactory;
 import types.ETFDatas;
 
+import java.time.Month;
+import java.util.List;
+import java.util.Map;
+
 public class ETFDataTest {
     public static void main (String[] args){
-        ETFDatas e = ETFDataFactory.readETFDataFile("data/EWD_test.csv");
-        System.out.println("Size: " + e.getSizeOfETFDatas());
-        System.out.println(e.toString());
+        // test of ETFDataFactory
+        ETFDatas etfds1 = ETFDataFactory.readETFDataFile("data/EWD_test.csv");
+        System.out.println("Size: " + etfds1.getNumberOfEtfDatas());
+
+        //  test allETFsUptrend()
+        System.out.println("Are all the ETFs in an uptrend? :" + etfds1.allETFsUptrend());
+
+        // test countUptrendETFs()
+        System.out.println("The number of ETFs in an uptrend: " + etfds1.countUptrendETFs());
+
+        //   filterUptrendETFs()
+        ETFDatas etfsfilt = etfds1.filterUptrendETFs();
+        System.out.println("Filtered ETFs, which are in an uptrend : " + etfsfilt.toString());
+
+        //  Test groupByMonth()
+        Map<Month, List<ETFData>> groupMonth1 = etfds1.groupByMonth();
+        for (Map.Entry<Month, List<ETFData>> entry : groupMonth1.entrySet()) {
+            System.out.println("Month: "+ entry.getKey() +", ETFs: " + entry.getValue());
+        }
+
+        //Test groupByMonthWithCount()
+        Map<Month, Long> groupMonthwCount1 = etfds1.groupByMonthWithCount();
+        for (Map.Entry<Month, Long> entry :groupMonthwCount1.entrySet()) {
+            System.out.println("Month: " + entry.getKey() + ", Number: " + entry.getValue());
+        }
     }
 }
+
